9597ae83c68da429b5bf89fc6e1b5cc8f788125d
Zhengyu Qian <laerhsif@gmail.com>
2019-12-11 10:38:44 +0800

    Make the Index template
    * Add templates.
    * Make styles.
    * Make view scripts.

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -9,12 +9,17 @@ _misc();
 
 
 // View
+import viewIndex from './view/index';
 import viewEssay from './view/essay';
 
 // View router
 let view = doc.querySelector('body').classList[0];
 
 switch (view) {
+	case 'index': {
+		viewIndex();
+		break;
+	}
 	case 'essay': {
 		viewEssay();
 		break;
---
+++ src/javascript/view/index.js
@@ -0,0 +1,22 @@
+/* global Han */
+
+export default function () {
+	
+let doc = document;
+// let win = window;
+
+
+
+// Init Han.css
+Han(doc.querySelector('.index-list'))
+.initCond()
+.renderElem()
+.renderHanging()
+.renderJiya()
+.renderHWS()
+.correctBasicBD()
+.substCombLigaWithPUA();
+
+
+
+}
